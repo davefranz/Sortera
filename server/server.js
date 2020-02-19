86780c5c56c746d8edb57361cc0ed0c896df9b4f
Dave Franz <davidlawrencefranz@gmail.com>
2020-02-19 12:20:56 -0800

    add express.json and removed body-parser

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
 // this is our server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const PORT = 3000;
-const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const dbController = require('./controllers/dbController')
+const dbController = require('./controllers/dbController');
 
-app.use(bodyParser.urlencoded( {extended: true} ));
+app.use(express.json());
 app.use(cookieParser());
 
 
 app.post('/createUser', dbController.createUser, (req, res) => {
-  res.status(200);
-})
+  res.sendStatus(200);
+});
 app.get('/', (req, res) => {
   res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'));
-})
+});
 
 // app.get('/build/bundle.js', (req, res) => {
 //   res.status(200).sendFile(path.resolve(__dirname, '../build/bundle.js'));
